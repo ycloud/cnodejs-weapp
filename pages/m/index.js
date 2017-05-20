b0731bac901d8ec951c0d9b86bb94c70e521ab73
ycloud <abc123@foxmail.com>
2017-05-20 14:34:29 +0800

    退出登录功能

--- pages/m/index.js
+++ pages/m/index.js
@@ -1,6 +1,16 @@
 // pages/m/index.js
 const app = getApp()
 Page({
+  signout() {
+    app.globalData.token = ''
+    app.globalData.account = null
+    wx.removeStorage({
+      key: 'token'
+    })
+    wx.reLaunch({
+      url: '/pages/m/index'
+    })
+  },
   onLoad() {
     let self = this
     if (app.auth()) {
